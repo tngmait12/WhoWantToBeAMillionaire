4d2568a60a762f3415966597f9efb46a752f4e4e
TruongVu <119274662+tngmait12@users.noreply.github.com>
2024-12-18 16:56:24 +0700

    EventClick

--- WhoWantToBeAMillionaire/obj/Debug/net8.0/WhoWantToBeAMillionaire.AssemblyInfo.cs
+++ WhoWantToBeAMillionaire/obj/Debug/net8.0/WhoWantToBeAMillionaire.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhoWantToBeAMillionaire")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e4a425a14b7641ba1e9133ccaa0d9b907b22c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebe403f26607a1d09ef7b7d32c2eaa2c2c727c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhoWantToBeAMillionaire")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhoWantToBeAMillionaire")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
